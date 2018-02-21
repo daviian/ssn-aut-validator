a39dbe6de388922349eef66dc70e76b84c32ab5b
David Schneiderbauer <dschneiderbauer@gmail.com>
2018-02-21 16:26:25 +0100

    add tests for ssn validation

---
+++ test/index_test.js
@@ -0,0 +1,27 @@
+/**
+ * Module dependencies.
+ */
+import isValid from '../src';
+
+/**
+ * `Social Security Number` samples.
+ */
+const numbers = {
+  invalid: ['1234010158', '1485040585', '8912121296', '12345678', '1234567898754'],
+  valid: ['6421310776', '4256080399', '9875210802', '3968280689']
+};
+
+/**
+ * Test `ssn-validator`.
+ */
+describe('SsnAUTValidator', () => {
+  describe('isValid()', () => {
+    it('should return `false` if number is invalid', () => {
+      numbers.invalid.forEach(number => isValid(number).should.be.false());
+    });
+
+    it('should return `true` if number is valid', () => {
+      numbers.valid.forEach(number => isValid(number).should.be.true());
+    });
+  });
+});
